187e44a898098896933d4ce7c2ce75f047a5a355
LOGEDI <nelsonlogedi@gmail.com>
2023-01-17 13:23:02 +0000

    final trafic

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,6 @@
 import React,{useState} from "react";
+//const [hidden, change] = useState("yes");
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,12 +9,27 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [selectedColor, setSelectedColor]=useState("red");
 	return (
-		<div className="trafic-light">
-			<div className={"light red"+(selectedColor==="red")?"glow":""}></div>
-			<div className={"light yellow"+(selectedColor==="yellow")?"glow":""}></div>
-			<div className={"light green"+(selectedColor==="green")?"glow":""}></div>
-	
+	<div className="container text-center">	
+	     <div className="tlTop"></div>
+		 <div className="trafic-light">
+			<div 
+			onClick={()=>setSelectedColor("red")}
+			className={
+				"light red " + (selectedColor==="red" ? "glow":"")}>
+            </div>
+			<div  
+			   onClick={()=>setSelectedColor("yellow")}
+			   className={
+				"light yellow " + (selectedColor==="yellow" ? "glow":"")}>
+
+				</div>
+			<div 
+			   onClick={ ()=>setSelectedColor("green")}
+			     className={ 
+				"light green " + (selectedColor==="green" ? "glow":"")}>
+         </div>
 		</div>
+	</div>
 	);
 };
 
